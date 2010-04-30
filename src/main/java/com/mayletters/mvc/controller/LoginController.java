565bdfccf31e60078ccf5558dc913788ccfa486d
aykutyararbas@yahoo.com <aykutyararbas@yahoo.com@0cb3cd61-4fe8-6f37-effb-24527adbc620>
2010-04-30 05:37:13 +0000

    login controller  see tiles-def.xml   user -->usermain   admin-->adminmain   kayıt-ol-->register

---
+++ src/main/java/com/mayletters/mvc/controller/LoginController.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package com.mayletters.mvc.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.ServletRequestUtils;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.ParameterizableViewController;
+
+/**
+ * @author aykutt
+ *
+ */
+public class LoginController extends ParameterizableViewController {
+
+	@Override
+	protected ModelAndView handleRequestInternal(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		//Login operation here
+		String username=request.getParameter("username");
+		ModelAndView mav = new ModelAndView();
+		if(username!=null&&username.trim().equals("admin")){
+				mav.setViewName("adminmain");
+		} else {
+			if(username!=null&&username.trim().equals("user")){
+				mav.setViewName("usermain");
+			}
+		}
+		if(username==null){
+			mav.setViewName("register");	
+		}
+	    return mav;
+	}
+
+}
