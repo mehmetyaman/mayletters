e4f10d91983c6d368c0ec09fec6b9099b9c7a6d7
mehmetyaman@gmail.com <mehmetyaman@gmail.com@0cb3cd61-4fe8-6f37-effb-24527adbc620>
2010-05-19 18:52:11 +0000

    correction for request mapping

--- src/main/java/com/mayletters/mvc/controller/RegistrationController.java
+++ src/main/java/com/mayletters/mvc/controller/RegistrationController.java
@@ -2,16 +2,19 @@ package com.mayletters.mvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class RegistrationController{
-	
-	@RequestMapping("/register.do")
-	public String redirect()
-	{
+@RequestMapping(value = "/register.do")
+public class RegistrationController {
+
+//	public String redirect() {
+//		return "/public/register";
+//	}
+
+	@RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
+	public String onSubmit() {
 		return "/public/register";
 	}
-
 
 }
