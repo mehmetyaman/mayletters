879b3d1ccae5040607681c033e4fbae8dc43fdc8
mehmetyaman@gmail.com <mehmetyaman@gmail.com@0cb3cd61-4fe8-6f37-effb-24527adbc620>
2010-06-06 09:25:47 +0000

    controller redirection changed

--- src/main/java/com/mayletters/mvc/controller/RegistrationController.java
+++ src/main/java/com/mayletters/mvc/controller/RegistrationController.java
@@ -1,20 +1,65 @@
 package com.mayletters.mvc.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Validator;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.mayletters.domain.User;
+import com.mayletters.service.dao.UserDao;
+
 @Controller
-@RequestMapping(value = "/register.do")
 public class RegistrationController {
+	
+	private Validator validator;
+	private UserDao userDao;
 
-//	public String redirect() {
-//		return "/public/register";
-//	}
-
-	@RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
-	public String onSubmit() {
+	@RequestMapping(value = "/register/showform.do")
+	public String showForm() {
 		return "/public/register";
 	}
+
+	@RequestMapping(value = "/register/register.do", method = RequestMethod.POST)
+	public String register(@ModelAttribute("user") User user,  BindingResult result ) {
+
+		validator.validate(user, result);
+		
+		userDao.persist(user);
+		
+		List<User> users = userDao.listByUserName(user.getUserName());
+		
+		if(users !=null && users.size()>0){
+			return "/public/register";
+		} else {
+			userDao.persist(user);
+		}
+
+		
+		if(result.hasErrors()){
+			// try again
+			return "/public/register";
+		} else {
+			// success
+			return "/public/main";
+		}
+		
+
+	}
+
+	public void setValidator(Validator validator) {
+		this.validator = validator;
+	}
+
+	/**
+	 * @param userDao the userDao to set
+	 */
+	public void setUserDao(UserDao userDao) {
+		this.userDao = userDao;
+	}
+
 
 }
